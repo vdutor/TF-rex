1eb6ac70ac81495934f5819fa3cc5d268dd42f02
vdutor <dutordoirv@gmail.com>
2017-03-07 18:28:11 +0100

    clean up the main

--- main.py
+++ main.py
@@ -1,40 +1,13 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from game_agent import GameAgent, Action
 from PIL import Image
-from io import BytesIO
-import SocketServer
-import base64
 import numpy as np
-from multiprocessing import Process, Queue
+import time
 
-cnt = 0
+agent = GameAgent('localhost', 9090)
+agent.startGame()
 
-class Handler(BaseHTTPRequestHandler):
-
-    def __init__(self, request, client_address, server):
-        BaseHTTPRequestHandler.__init__(self, request, client_address, server)
-
-    def _set_headers(self):
-        self.send_response(200)
-        self.end_headers()
-
-    def do_POST(self):
-        global cnt, prev_image
-        self._set_headers()
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        data = str(post_data)
-        data = data[data.index(',')+1:]
-        im = np.array(Image.open(BytesIO(base64.b64decode(data))))
-
-        # for h in range(im.shape[0]):
-            # for w in range(im.shape[1]):
-                # if any(im[h,w,:]) != 0:
-                    # print im[h,w,:]
-
-        # Image.fromarray(im).save("figure_" +str(cnt) + ".png")
-        # cnt += 1
-
-address = ('localhost', 9090)
-server = HTTPServer(address, Handler)
-print "listening..."
-server.serve_forever()
+for cnt in range(20):
+    image, reward, crashed = agent.doAction(Action.UP)
+    print crashed
+    # Image.fromarray(image).save("figure_test_" + str(cnt) + ".png")
+    # time.sleep(.3)
